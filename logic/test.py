cd1603a3b846c04c458e24064b9a732fb29952c3
tatsuji_ho <longer271271@gmail.com>
2022-06-17 14:42:08 +0900

    動画をデコードしてtmpに格納できるようになった

--- logic/test.py
+++ logic/test.py
@@ -6,7 +6,9 @@ def test():
     with open("./logic/ENCODED_MP4.txt", "r") as f :
         encoded_str = f.readline()
             
-    video_binary = decode_to_video(encoded_str)    
+    video_name = "2.mp4"
+            
+    decode_to_video(encoded_str, video_name)
 
 if __name__ == "__main__" :
     test()
