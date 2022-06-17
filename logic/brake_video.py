43c8d98058859dc0aa7ca2c31f03418b45cf9b67
tatsuji_ho <longer271271@gmail.com>
2022-06-17 15:03:49 +0900

    動画をフレームに切り出せる
    tmp直下の動画ファリルと同じ名前のディレクトリにおく
    連番がアンダースコアの後に書かれている

--- logic/brake_video.py
+++ logic/brake_video.py
@@ -1,7 +1,37 @@
-def brake_video(video):
+import cv2
+import os
+
+def brake_video(video_name=""):
     '''
     parameters
     ----------
-    video
+    video_name : str
+        動画のファイル名
+    returns
+    -------
+    None
     '''
-    pass
+    
+    PATH_TO_TMP = "./tmp/"
+    
+    filepath = PATH_TO_TMP + video_name
+    capture = cv2.VideoCapture(filepath)
+    if not capture.isOpened():
+        return
+    
+    base_name = "".join(video_name.split(".")[:-1])
+    dir_path = PATH_TO_TMP + base_name
+    os.makedirs(dir_path, exist_ok=True)
+    base_path = os.path.join(dir_path, base_name)
+    
+    digit = len(str(int(capture.get(cv2.CAP_PROP_FRAME_COUNT))))
+    
+    n = 0
+    EXT = "png"
+    while True:
+        ret, frame = capture.read()
+        if ret:
+            cv2.imwrite('{}_{}.{}'.format(base_path, str(n).zfill(digit), EXT), frame)
+            n += 1
+        else:
+            return
