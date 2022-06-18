970645c255bab9fad6114a266df82e5326d648ad
tatsuji_ho <longer271271@gmail.com>
2022-06-18 11:53:27 +0900

    フレームを抽出する関数のガワをつくった

---
+++ logic/pick_frame.py
@@ -0,0 +1,12 @@
+def is_frame_to_use(img_path) :
+    '''
+    parameters
+    ----------
+    img_path : str
+        画像までのパス
+    
+    returns
+    -------
+    is_yes : bool
+        そのフレームを使うならTrue
+    '''
