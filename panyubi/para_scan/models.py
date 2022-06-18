a73173845aa9d82639aebc9216635a128de03396
tomoki52 <konishi.tomoki.ap@alumni.tsukuba.ac.jp>
2022-06-18 15:32:53 +0900

    動画アップロードして保存できるようになった

--- panyubi/para_scan/models.py
+++ panyubi/para_scan/models.py
@@ -3,12 +3,11 @@ from django.db import models
 # Create your models here.
 
 class Media(models.Model):
-    id = models.UUIDField
+    #id = models.UUIDField
     video = models.FileField(upload_to="video/")
 
 
 class Result(models.Model):
     media = models.ForeignKey(Media, on_delete=models.CASCADE, related_name="results")
-    id = models.UUIDField
+    #id = models.UUIDField
     content = models.FileField(upload_to="result/")
-
--- panyubi/para_scan/urls.py
+++ panyubi/para_scan/urls.py
@@ -2,9 +2,10 @@ from django.urls import path,include
 from rest_framework import routers
 from . import views
 
+router = routers.DefaultRouter()
+router.register(r'upload', views.MediaViewSet, basename='medias')
+
 urlpatterns = [
     path(
-        'api/get/',
-        views.GetOcr.as_view(),
-        name='cls-v'),
+        'api/', include(router.urls)),
 ]
